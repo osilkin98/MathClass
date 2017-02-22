f6cc1fb908635ff18d1cacf5a6bd1d8992079152
osilkin98 <o.silkin98@gmail.com>
2017-02-21 22:36:04 -0600

    Create simpson.py

---
+++ simpson.py
@@ -0,0 +1,15 @@
+
+def approx(f, a, b): # to approximate area of func f from a --> b by Simpson
+  ar = ((b - a) / 6) * (f(a) + 4 * f((a - b) / 2) + f(b))
+  return ar
+
+def simpson_sum(f, a, b, n): # sums up approximations on interval a to b of function f
+  
+  s, diff = 0, (b - a)/n
+  
+  i = a
+  while((i <= b and b > a) or (a > b and i <= a)):
+    s += approx(f, i, i + diff)
+    i += diff
+  
+  return s
